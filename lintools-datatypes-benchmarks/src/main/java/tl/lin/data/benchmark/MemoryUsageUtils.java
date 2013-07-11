eed94d16e5572631f7183ab06cbd66ae228b7add
lintool <jimmylin@umd.edu>
2013-07-11 14:21:53 +0200

    Code extraction from Cloud9.

---
+++ lintools-datatypes-benchmarks/src/main/java/tl/lin/data/benchmark/MemoryUsageUtils.java
@@ -0,0 +1,33 @@
+package tl.lin.data.benchmark;
+
+public class MemoryUsageUtils {
+
+  public static long getUsedMemory() {
+    return Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+  }
+
+  public static long getUsedMemoryAccurate() {
+    gc();
+    long totalMemory = Runtime.getRuntime().totalMemory();
+    gc();
+    long freeMemory = Runtime.getRuntime().freeMemory();
+    long usedMemory = totalMemory - freeMemory;
+    return usedMemory;
+  }
+
+  public static void gc() {
+    try {
+      System.gc();
+      Thread.sleep(100);
+      System.runFinalization();
+      Thread.sleep(100);
+      System.gc();
+      Thread.sleep(100);
+      System.runFinalization();
+      Thread.sleep(100);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+}
