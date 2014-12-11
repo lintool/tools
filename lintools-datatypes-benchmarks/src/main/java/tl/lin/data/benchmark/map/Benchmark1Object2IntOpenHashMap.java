ed6d90ff580e13e5742849a645209a5664cc6993
lintool <jimmylin@umd.edu>
2014-12-10 22:47:57 -0500

    tabs -> spaces

--- lintools-datatypes-benchmarks/src/main/java/tl/lin/data/benchmark/map/Benchmark1Object2IntOpenHashMap.java
+++ lintools-datatypes-benchmarks/src/main/java/tl/lin/data/benchmark/map/Benchmark1Object2IntOpenHashMap.java
@@ -8,44 +8,44 @@ import tl.lin.data.benchmark.MemoryUsageUtils;
 
 public class Benchmark1Object2IntOpenHashMap {
 
-	public static void main(String[] args) {
-		int size = 5000000;
-		long startTime;
-		long duration;
-		Random r = new Random();
-		int[] ints = new int[size];
+  public static void main(String[] args) {
+    int size = 5000000;
+    long startTime;
+    long duration;
+    Random r = new Random();
+    int[] ints = new int[size];
 
-		long usedMemory1 = MemoryUsageUtils.getUsedMemory();
+    long usedMemory1 = MemoryUsageUtils.getUsedMemory();
 
-		System.out.println("Benchmarking Object2IntOpenHashMap<String>...");
-		Object2IntOpenHashMap<String> map = new Object2IntOpenHashMap<String>();
+    System.out.println("Benchmarking Object2IntOpenHashMap<String>...");
+    Object2IntOpenHashMap<String> map = new Object2IntOpenHashMap<String>();
 
-		startTime = System.currentTimeMillis();
-		for (int i = 0; i < size; i++) {
-			int k = r.nextInt(size);
-			map.put("" + i, k);
-			ints[i] = k;
-		}
-		duration = System.currentTimeMillis() - startTime;
-		System.out.println(" Inserting " + size + " random entries: " + duration + " ms");
+    startTime = System.currentTimeMillis();
+    for (int i = 0; i < size; i++) {
+      int k = r.nextInt(size);
+      map.put("" + i, k);
+      ints[i] = k;
+    }
+    duration = System.currentTimeMillis() - startTime;
+    System.out.println(" Inserting " + size + " random entries: " + duration + " ms");
 
-		startTime = System.currentTimeMillis();
-		for (int i = 0; i < size; i++) {
-			int v = map.getInt("" + i);
+    startTime = System.currentTimeMillis();
+    for (int i = 0; i < size; i++) {
+      int v = map.getInt("" + i);
 
-			if (v != ints[i])
-				throw new RuntimeException("Values don't match!");
-		}
-		duration = System.currentTimeMillis() - startTime;
-		System.out.println(" Accessing " + size + " random entries: " + duration + " ms");
+      if (v != ints[i])
+        throw new RuntimeException("Values don't match!");
+    }
+    duration = System.currentTimeMillis() - startTime;
+    System.out.println(" Accessing " + size + " random entries: " + duration + " ms");
 
-		long usedMemory2 = MemoryUsageUtils.getUsedMemory();
+    long usedMemory2 = MemoryUsageUtils.getUsedMemory();
 
-		System.out.println("Used memory before: " + usedMemory1);
-		System.out.println("Used memory after: " + usedMemory2);
-		System.out.println("Total memory usage: " + (usedMemory2 - usedMemory1));
-		System.out.println("Memory usage per map entry: "
-				+ ((float) (usedMemory2 - usedMemory1) / size));
-	}
+    System.out.println("Used memory before: " + usedMemory1);
+    System.out.println("Used memory after: " + usedMemory2);
+    System.out.println("Total memory usage: " + (usedMemory2 - usedMemory1));
+    System.out.println("Memory usage per map entry: "
+        + ((float) (usedMemory2 - usedMemory1) / size));
+  }
 
 }
--- lintools-datatypes-benchmarks/src/main/java/tl/lin/data/benchmark/map/Benchmark2Int2IntOpenHashMap.java
+++ lintools-datatypes-benchmarks/src/main/java/tl/lin/data/benchmark/map/Benchmark2Int2IntOpenHashMap.java
@@ -8,44 +8,44 @@ import tl.lin.data.benchmark.MemoryUsageUtils;
 
 public class Benchmark2Int2IntOpenHashMap {
 
-	public static void main(String[] args) {
-		int size = 5000000;
-		long startTime;
-		long duration;
-		Random r = new Random();
-		int[] ints = new int[size];
+  public static void main(String[] args) {
+    int size = 5000000;
+    long startTime;
+    long duration;
+    Random r = new Random();
+    int[] ints = new int[size];
 
-		long usedMemory1 = MemoryUsageUtils.getUsedMemory();
+    long usedMemory1 = MemoryUsageUtils.getUsedMemory();
 
-		System.out.println("Benchmarking Int2IntOpenHashMap...");
-		Int2IntOpenHashMap map = new Int2IntOpenHashMap();
+    System.out.println("Benchmarking Int2IntOpenHashMap...");
+    Int2IntOpenHashMap map = new Int2IntOpenHashMap();
 
-		startTime = System.currentTimeMillis();
-		for (int i = 0; i < size; i++) {
-			int k = r.nextInt(size);
-			map.put(i, k);
-			ints[i] = k;
-		}
-		duration = System.currentTimeMillis() - startTime;
-		System.out.println(" Inserting " + size + " random entries: " + duration + " ms");
+    startTime = System.currentTimeMillis();
+    for (int i = 0; i < size; i++) {
+      int k = r.nextInt(size);
+      map.put(i, k);
+      ints[i] = k;
+    }
+    duration = System.currentTimeMillis() - startTime;
+    System.out.println(" Inserting " + size + " random entries: " + duration + " ms");
 
-		startTime = System.currentTimeMillis();
-		for (int i = 0; i < size; i++) {
-			int v = map.get(i);
+    startTime = System.currentTimeMillis();
+    for (int i = 0; i < size; i++) {
+      int v = map.get(i);
 
-			if (v != ints[i])
-				throw new RuntimeException("Values don't match!");
-		}
-		duration = System.currentTimeMillis() - startTime;
-		System.out.println(" Accessing " + size + " random entries: " + duration + " ms");
+      if (v != ints[i])
+        throw new RuntimeException("Values don't match!");
+    }
+    duration = System.currentTimeMillis() - startTime;
+    System.out.println(" Accessing " + size + " random entries: " + duration + " ms");
 
-		long usedMemory2 = MemoryUsageUtils.getUsedMemory();
+    long usedMemory2 = MemoryUsageUtils.getUsedMemory();
 
-		System.out.println("Used memory before: " + usedMemory1);
-		System.out.println("Used memory after: " + usedMemory2);
-		System.out.println("Total memory usage: " + (usedMemory2 - usedMemory1));
-		System.out.println("Memory usage per map entry: "
-				+ ((float) (usedMemory2 - usedMemory1) / size));
-	}
+    System.out.println("Used memory before: " + usedMemory1);
+    System.out.println("Used memory after: " + usedMemory2);
+    System.out.println("Total memory usage: " + (usedMemory2 - usedMemory1));
+    System.out.println("Memory usage per map entry: "
+        + ((float) (usedMemory2 - usedMemory1) / size));
+  }
 
 }
